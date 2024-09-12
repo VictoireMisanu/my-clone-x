7f67394b9f579c4435898e51da8644ac1b6dbfd7
Vic <vicmisanu63@gmail.com>
2024-09-12 15:34:58 +0200

    Trend part title resized

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -5,7 +5,7 @@ import NavComponent from "../SideNav/NavComponent/NavComponent"
 function Title(){
     return(
         <div className="flex flex-row justify-between">
-            <Text text="Trends for you" className="text-white text-sm font-bold"/>
+            <Text text="Trends for you" className="text-white text-xl font-bold"/>
             <NavComponent src="/logo/settings.svg"/>
         </div>
     )
